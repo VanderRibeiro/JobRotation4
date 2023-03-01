60671f4139e96925e7ccf9ef0a60eb912be8d18d
VanderRibeiro <vbruno383@gmail.com>
2023-02-28 23:23:21 -0300

    último co,mit

---
+++ JobRotationTest4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Escreva uma palavra para ser invertida: ");
+        string palavra = Console.ReadLine();
+
+        char[] ctr = palavra.ToCharArray();
+        Array.Reverse(ctr);
+        palavra = new string(ctr);
+        Console.WriteLine(palavra);
+    }
+}
